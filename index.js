c342450849b0a93406b152e55f3210ad7dadcd32
fed135 <fredericcharette@gmail.com>
2017-04-07 17:12:12 -0400

    fixed formatting, bumped to kalmv2

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ const send_options = { binary: true };
  * @returns {Buffer} The resulting Buffer
  */
 function _abToBuffer(ab) {
-	let buffer = Buffer.allocUnsafe(ab.byteLength || ab.data.length);
-	let view = ab.data || new Uint8Array(ab);
-	const len = buffer.length;
-	for (let i = 0; i < len; i++) {
-		buffer[i] = view[i];
-	}
-	return buffer;
+ let buffer = Buffer.allocUnsafe(ab.byteLength || ab.data.length);
+ let view = ab.data || new Uint8Array(ab);
+ const len = buffer.length;
+ for (let i = 0; i < len; i++) {
+   buffer[i] = view[i];
+ }
+ return buffer;
 }
 
 /**
@@ -38,15 +38,11 @@ function _abToBuffer(ab) {
  * @param {Server} server The Kalm Server reference
  * @param {function} callback The success callback for the operation
  */
-function listen(server, callback) {
-	server.listener = new ws.Server({
-		port: server.options.port
-	}, callback);
-
-	server.listener.on('connection', server.handleRequest.bind(server));
-	server.listener.on('error', (err) => {
-		server.emit('error', err);
-	});
+function listen(handlers, options) {
+  const listener = new ws.Server({ port: options.port });
+  listener.on('connection', handlers.handleConnection);
+  listener.on('error', handlers.handleError);
+  return Promise.resolve(listener);
 }
 
 /**
@@ -54,11 +50,11 @@ function listen(server, callback) {
  * @param {Socket} socket The socket to use
  * @param {Buffer} payload The body of the request
  */
-function send(socket, payload, timestamp) {
-	if (socket && socket.sendBytes) socket.sendBytes(payload);
-	else {
-		if (socket) socket.send(payload, send_options);
-	}
+function send(socket, payload) {
+  if (socket.sendBytes) socket.sendBytes(payload);
+  else {
+    socket.send(payload, send_options);
+  }
 }
 
 /**
@@ -67,8 +63,8 @@ function send(socket, payload, timestamp) {
  * @param {function} callback The success callback for the operation
  */
 function stop(server, callback) {
-	server.listener.close();
-	setTimeout(callback, 0);
+  server.listener.close();
+  setTimeout(callback, 0);
 }
 
 /**
@@ -76,55 +72,56 @@ function stop(server, callback) {
  * @param {Client} client The client to create the socket for
  * @returns {Socket} The created WebSocket client
  */
-function createSocket(client, socket) {
-	if (!socket) {
-		socket = new ws('ws://' + client.options.hostname + ':' + client.options.port);
-		if (is_browser) socket.binaryType = 'arraybuffer';
-	}
+function createSocket(client) {
+  const socket = new ws(`ws://${client.hostname}:${client.port}`);
+  socket.binaryType = 'nodebuffer';
+  socket.__pending = true;
 
-	if (is_browser) {
-		socket.onmessage = (evt) => {
-			client.handleRequest(_abToBuffer(evt.data));	// Browser ArrayBuffer to node Buffer
-		};
+  return socket;
+}
 
-		socket.onerror = client.handleError.bind(client);
+function getOrigin(socket) {
+  return {
+    host: socket._socket.remoteAddress,
+    port: socket._socket.remotePort
+  };
+}
 
-		socket.onclose = client.handleDisconnect.bind(client);
-
-		socket.onopen = client.handleConnect.bind(client);
-	}
-	else {
-		socket.on('message', (evt) => {
-			client.handleRequest(_abToBuffer(evt));	// Browser ArrayBuffer to node Buffer
-		});
-
-		socket.on('error', client.handleError.bind(client));
-
-		socket.on('connect', client.handleConnect.bind(client));
-
-		socket.on('close', client.handleDisconnect.bind(client));
-	}
-
-	return socket;
+function attachSocket(socket, handlers) {
+  if (is_browser) {
+    socket.onmessage = evt => handlers.handleRequest(_abToBuffer(evt.data));
+    socket.onerror = handlers.handleError;
+    socket.onclose = handlers.handleDisconnect;
+    socket.onopen = handlers.handleConnect;
+  }
+  else {
+    socket.on('message', evt => handlers.handleRequest(_abToBuffer(evt)));
+    socket.on('error', handlers.handleError);
+    socket.on('close', handlers.handleDisconnect);
+    if (socket.__pending) {
+      delete socket.__pending;
+      socket.on('open', handlers.handleConnect);
+    }
+  }
 }
 
 /**
  * Attempts to disconnect the socket
  * @param {Socket} socket The socket to disconnect
  */
-function disconnect(client) {
-	if (client.socket && client.socket.disconnect) {
-		client.socket.disconnect();
-		setTimeout(client.handleDisconnect.bind(client), 0);
-	}
+function disconnect(client, callback) {
+  client.socket.close();
+  setTimeout(callback, 0);
 }
 
 /* Exports -------------------------------------------------------------------*/
 
 module.exports = {
-	listen,
-	send,
-	createSocket,
-	stop,
-	disconnect
+  listen,
+  send,
+  createSocket,
+  attachSocket,
+  getOrigin,
+  stop,
+  disconnect
 };
--- tests/index.js
+++ tests/index.js
@@ -8,60 +8,22 @@
 
 const assert = require('chai').assert;
 const Kalm = require('kalm');
-const websocket = require('../index');
-
-/* Models --------------------------------------------------------------------*/
-
-const adapterFormat = {
-	listen: function() {},
-	send: function() {},
-	stop: function() {},
-	createSocket: function() {},
-	disconnect: function() {}
-};
+const ws = require('../index');
 
 /* Suite ---------------------------------------------------------------------*/
 
-describe('Adapters', () => {
+describe('Websocket', () => {
 
-	describe('methods', () => {
-		it('register', () => {
-			Kalm.adapters.register('ws', websocket);
-		});
+  it('run ws + json', (done) => {
+    const server = Kalm.listen({ transport: ws, port:8000 });
+    server.on('connection', c => {
+      c.subscribe('test', (data) => {
+        assert.deepEqual(data.body, {foo:'bar'});
+        server.stop(done);
+      });
+    });
 
-		it('resolve', () => {
-			let ws_test = Kalm.adapters.resolve('ws');
-			assert.isObject(ws_test, 'ws is not a valid adapter object');
-			allMembersTypeMatch(ws_test, adapterFormat);
-		});
-	});
-
-	describe('Smoke test', () => {
-		it('run ws + json', (done) => {
-			let server = new Kalm.Server({ adapter:'ws', port:8000 });
-			server.subscribe('test', (data) => {
-				assert.deepEqual(data, {foo:'bar'});
-				server.stop(done);
-			});
-
-			server.on('ready', () => {
-				let client = new Kalm.Client({ adapter:'ws', port:8000 });
-				client.send('test', {foo:'bar'});
-			});
-		});
-	});
+    const client = Kalm.connect({ transport: ws, port:8000 });
+    client.write('test', {foo:'bar'});
+  });
 });
-
-/* Tooling -------------------------------------------------------------------*/
-
-/**
- * Checks that all properties are present and of the proper type
- */
-function allMembersTypeMatch(set1, model) {
-	for (let i in model) {
-		let type = typeof model[i];
-		assert.property(set1, i, 'property ' + i + ' is missing');
-		assert.typeOf(set1[i], type, 'property ' + i + ' should be ' + type);
-	}
-	return true;
-}
